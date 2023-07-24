73d6c42dfe578d9406bd2b13dc6e88b09adedd40
Istvan Sarandi <sarandi@vision.rwth-aachen.de>
2023-07-24 22:21:41 +0200

    Update for conda

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from setuptools import setup
+import os
+
+try:
+    dependencies_managed_by_conda = os.environ['DEPENDENCIES_MANAGED_BY_CONDA'] == '1'
+except KeyError:
+    dependencies_managed_by_conda = False
 
 setup(
     name='boxlib',
@@ -9,5 +15,5 @@ setup(
     license='LICENSE',
     description='Utilities for bounding box manipulation',
     python_requires='>=3.6',
-    install_requires=['numpy']
+    install_requires=[] if dependencies_managed_by_conda else ['numpy']
 )
